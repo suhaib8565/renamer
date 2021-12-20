5eb8849e98e2ca374305f29b6fc1dbce7914ed72
Pʀᴇᴅᴀᴛᴏʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2021-12-20 10:56:12 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,8 +9,8 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     SESSION_NAME = os.environ.get("SESSION_NAME", "Rename-Bot-0")
     SLEEP_TIME = int(os.environ.get("SLEEP_TIME", 5))
-    BOT_OWNER = os.environ.get("BOT_OWNER", 1445283714)
-    CAPTION = "Rename Bot by @{}\n\nMade by @AbirHasan2005"
+    BOT_OWNER = os.environ.get("BOT_OWNER", 1287407305)
+    CAPTION = "Rename Bot by @{}\n\nMade by @KDramasFlix"
     UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
